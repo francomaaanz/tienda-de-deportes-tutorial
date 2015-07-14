c4d06f1b18f9246d3bdf652baf8646c63decb856
francomaaanz <francomanz@hotmail.com>
2015-07-14 20:13:18 -0300

    project commit

---
+++ sports/public/controllers/sportsStore.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("sportsStore")
+.constant("dataUrl", "http://localhost:2403/productos/")
+.controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+
+    $scope.data = {};
+
+    $http.get(dataUrl)
+        .sucess(function (data) {
+            $scope.data.productos = data;
+        })
+        .error(function (error) {
+            $scope.data.error = error;
+        });
+});
